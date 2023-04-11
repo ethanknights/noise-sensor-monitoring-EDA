2e6aee123a11c1fb96bfd34c76a72c86a93ef195
ethanknights <ethanknights@hotmail.co.uk>
2023-04-11 15:00:22 +0100

    WIP: refactor for importable functions

--- src/identify_top5BuildingSites.py
+++ src/identify_top5BuildingSites.py
@@ -1,4 +1,11 @@
-df_buildingComplaints = df[df['Complaint Type'] == 'Building Site']
+# Purpose: Identify building site address info for top 5(?) sites (i.e. highest N registered complaints)
+
+#
+df = rawD  # Inherited dataframe sorted byDate (from setup/setup.py)
+
+
+# Filter
+df = df[df['Complaint Type'] == 'Building Site']
 tmp = tmp.sort_values('Received Date')
 
 # add regexp with 2022
--- src/setup/setup.py
+++ src/setup/setup.py
@@ -1,23 +1,22 @@
 import pandas as pd
-import numpy as np
-import sys
-import datetime
 
-# Read
-rawD = pd.read_csv('./data/raw_April2023_1997-2023.csv')
-df = rawD.sort_values('Received Date')
-del rawD  # memory!
+def readData():
+    # Read
+    rawD = pd.read_csv('./data/raw_April2023_1997-2023.csv')
+    rawD = rawD.sort_values('Received Date')
 
-# Print unique values to get dataset gist
-unique_values_dict = {}
-for col in df.columns:
-    unique_values_dict[col] = df[col].unique()
-# print("\n\n\nUnique values in column {}: {}\n".format(col, unique_values_dict))
-# sys.getsizeof(unique_values_dict)
-print(unique_values_dict['Complaint Type Code'])
-print(unique_values_dict['Complaint Type'])
-print(unique_values_dict['Complaint Sub Type'])
-print(unique_values_dict['AddressKey'][0:9])
+    # Print unique values to get dataset gist
+    unique_values_dict = {}
+    for col in rawD.columns:
+        unique_values_dict[col] = rawD[col].unique()
+    # print("\n\n\nUnique values in column {}: {}\n".format(col, unique_values_dict))
+    # sys.getsizeof(unique_values_dict)
+    print(unique_values_dict['Complaint Type Code'])
+    print(unique_values_dict['Complaint Type'])
+    print(unique_values_dict['Complaint Sub Type'])
+    print(unique_values_dict['AddressKey'][0:9])
 
-# Analysis scripts next:
-print('Next:\npython ../identify_top5BuildingSites.py')
+    # Analysis scripts next:
+    print('Next:\npython ../identify_top5BuildingSites.py')
+
+    return rawD
