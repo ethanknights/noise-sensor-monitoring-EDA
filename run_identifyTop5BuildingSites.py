d5b8e621c67d82ccdc70ee866059f648ffc49507
ethanknights <ethanknights@hotmail.co.uk>
2023-04-11 15:20:02 +0100

    enhance: partition read_data() and print_unique_data()

--- run_identifyTop5BuildingSites.py
+++ run_identifyTop5BuildingSites.py
@@ -1,14 +1,14 @@
 # Purpose: Identify building site address info for top 5(?) sites (i.e. highest N registered complaints)
 import pandas as pd
-from setup import readData
+from setup import read_data
 
 # Read
-df = readData()  # Could be persistent but data is big: rawD = readData()
+df = read_data()  # Could be persistent but data is big: rawD = readData()
 
-# Filter
+# Filter to 'Building Site complaints
 df = df[df['Complaint Type'] == 'Building Site']
-tmp = tmp.sort_values('Received Date')
+# df = df.sort_values('Received Date')
 
 # add regexp with 2022
-tmp = tmp[tmp['Received Date'][7:] == '2022']
+# tmp = tmp[tmp['Received Date'][7:] == '2022']
 # len(tmp)
--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 import pandas as pd
+import sys
 # Inherited
-def readData():
+def read_data():
     # Return dataframe of all complaints (sorted byDate)
 
-    # Read
     rawD = pd.read_csv('./data/raw_April2023_1997-2023.csv')
     rawD = rawD.sort_values('Received Date')
 
-    # EXTRA TEMPORARY! Print unique values to get dataset gist
+    return rawD
+
+
+# EXTRA FUNCTIONS
+def extra_print_unique_data(df):
+    # EXTRA TEMPORARY!
+    # Print unique values to get dataset gist
     unique_values_dict = {}
-    for col in rawD.columns:
-        unique_values_dict[col] = rawD[col].unique()
-    # print("\n\n\nUnique values in column {}: {}\n".format(col, unique_values_dict))
-    # sys.getsizeof(unique_values_dict)
+    for col in df.columns:
+        unique_values_dict[col] = df[col].unique()
+    print("\n\n\nUnique values in column {}: {}\n".format(col, unique_values_dict))
+    sys.getsizeof(unique_values_dict)
     print(unique_values_dict['Complaint Type Code'])
     print(unique_values_dict['Complaint Type'])
     print(unique_values_dict['Complaint Sub Type'])
     print(unique_values_dict['AddressKey'][0:9])
 
-    # Analysis scripts next:
-    print('Next:\npython ../identify_top5BuildingSites.py')
-
-    return rawD
+    return
