0975d9288b714a4586e7266e08279db8aeb50815
ethanknights <ethanknights@hotmail.co.uk>
2023-04-12 09:54:48 +0100

    feat: add unit_test - setup.create_joined_datetime

--- unit_tests/test_setup.py
+++ unit_tests/test_setup.py
@@ -9,7 +9,7 @@ class TestDataFrame(unittest.TestCase):
     def test_dtype_convert_received_datetime(self):
         """
         Test that 'Received Date' & 'Received Time' columns in the input DataFrame are
-        correctly converted to datetime.time objects.
+        correctly converted to datetime.date/time/datetime objects.
 
         - First, it reads input data using the 'setup.read_data' function and asserts all column values are strings.
         - Next, it applies the 'setup.convert_raw_date_time' function to convert the string columns to datetime objects.
@@ -17,6 +17,7 @@ class TestDataFrame(unittest.TestCase):
             (1) All date values in the 'Received Date' column are datetime.date objects.
             (2) All non-null time values in the 'Received Time' column are datetime.time objects.
                 Non-null are used due to some missing data represented as 'NaT'
+        - An additional assertion is included for the 'Received DateTime' column.
 
         If the test fails, an AssertionError is raised.
         """
@@ -32,6 +33,10 @@ class TestDataFrame(unittest.TestCase):
         self.assertTrue(df['Received Time'].apply(lambda x: isinstance(x, datetime.time) if pd.notna(x) else True).all(),
                         "df['Received Time'] contains values that are not of type datetime.time")
 
+        df = setup.remove_missing_date_rows(df)
+        df = setup.create_joined_datetime(df)
+        self.assertTrue(df['Received DateTime'].apply(lambda x: isinstance(x, datetime.datetime)).all(),
+                        "df['Received Date'] contains values that are not of type datetime.datetime")
 
 if __name__ == '__main__':
     unittest.main()
